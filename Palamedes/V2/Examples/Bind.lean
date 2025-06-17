d01023dd9b25f9569a314f8ce32bc8ccd6255c88
Harrison Goldstein <hgoldstein95@gmail.com>
2025-06-17 18:03:40 -0400

    Got bind to work

--- Palamedes/V2/Examples/Bind.lean
+++ Palamedes/V2/Examples/Bind.lean
@@ -4,110 +4,72 @@ import Mathlib.Lean.Expr
 import Lean.Elab.Tactic.NormCast
 
 open Gen CorrectGen
+open Lean Meta Elab Tactic Term
 
-open Lean Meta Elab Tactic Term in
-elab "partitionResult" : tactic => do
-  let rec get_conjuncts (e: Expr) : TacticM $ (List Expr) := do
-    --if let (Expr.app (Expr.app (Expr.const `And _) p) q) := e then
-    if e.isAppOf `And then
-      --properly deconstructing the ∧ for termination to kick in
-      let (Expr.app (Expr.app (Expr.const `And _) p) q) := e | throwUnsupportedSyntax
-      let lhs_conjuncts ← get_conjuncts p -- e.getAppArgs[0]!
-      let rhs_conjuncts ← get_conjuncts q -- e.getAppArgs[1]!
-      return lhs_conjuncts ++ rhs_conjuncts
-    else if e.containsConst (λ c => c == `Exists) then
-      dbg_trace "contains exists"
-      throwUnsupportedSyntax -- TODO: better error
+/-- Extracts the conjuncts of an ∧-expression. -/
+private partial def getConjuncts (e: Expr) : TacticM (List Expr) := do
+  match_expr e with
+  | And p q => do
+    let lhs_conjuncts ← getConjuncts p
+    let rhs_conjuncts ← getConjuncts q
+    return lhs_conjuncts ++ rhs_conjuncts
+  | _ => return [e]
+
+/-- Partitions a list of expressions based on whether or not they use a given `FVarId`.  -/
+private def partitionContainsFVar (var : FVarId) (exprs : List Expr) : List Expr × List Expr :=
+  match exprs with
+  | .nil => ([], [])
+  | .cons e es =>
+    let (rest_t,rest_f) := partitionContainsFVar var es
+    if e.containsFVar var then
+      (e :: rest_t, rest_f)
     else
-      -- logInfo e.ctorName
-      return [e]
+      (rest_t, e :: rest_f)
 
-  let rec partition (exprs : List Expr) : (List Expr) × (List Expr) :=
-      match exprs with
-      | .nil => ([],[])
-      | .cons e es =>
-        let (rest_t,rest_f) := partition es
-        if e.hasLooseBVar 1 then
-        --if Expr.containsFVar e id then
-          (e :: rest_t,rest_f)
-        else (rest_t, e :: rest_f)
+/-- Proves a goal of the form `e = e'` by partitioning the conjuncts in the RHS based on whether or
+  not they contain `v`.
 
-  let mvarId ← getMainGoal
-  mvarId.withContext do
-    let mainDecl ← getMainDecl
-    let e := mainDecl.type
-    logInfo e.ctorName
-    if e.isAppOf `Eq then
-      --dbg_trace f!"maindecl: {t.isAppOf `CGen}"
-      let origOuterLam := e.getAppArgs[2]!
-      let (Expr.lam outer_name outer_type t outer_binfo) := origOuterLam | throwUnsupportedSyntax
-      -- outer_name is definitely #1!!! otherwise there will be an error due to structure
-      if t.isAppOf `Exists then
-        let (Expr.lam name type body binfo) := t.getAppArgs[1]! | throwUnsupportedSyntax
-        let conjuncts ← get_conjuncts body
-        logInfo conjuncts
-        -- let (Expr.bvar yidx) := conjuncts[0]!.getAppArgs[3]! | throwUnsupportedSyntax
-        -- logInfo f!"yidx: {yidx} {conjuncts[0]!.hasLooseBVar (yidx + 1)}"
+  E.g., the goal `?a ∧ ?b = v < 3 ∧ x = 5 ∧ 1 < v` is solved with
+  ```
+  ?a = (x = 5)
+  ?b = (v < 3 ∧ 1 < v)
+  ```
+  -/
+elab "partition_conjuncts " v:ident : tactic =>
+  withMainContext do
+    let g ← getMainGoal
+    let goalDecl ← g.getDecl
+    let goalTy := goalDecl.type
+    let var ← elabAsFVar v
+    match_expr goalTy with
+    | Eq _α _lhs rhs =>
+      -- Get and partition the conjuncts
+      let conjuncts ← getConjuncts rhs
+      let (varIn, varNotIn) := partitionContainsFVar var conjuncts
 
-        --let id ← outer_name.fvarId!
-        let (varIn,varNotIn) := partition conjuncts
-        logInfo f!"{varIn}/{varNotIn}"
-        if varIn.length < 1 then throwUnsupportedSyntax
-        let nameIn := mkAndN $ varIn
-        --logInfo nameIn
-        let rest := mkAndN $ varNotIn
-        --logInfo rest
-        let allAnd := mkAnd rest nameIn
-        --Reconstruct the exists:
-        let newLam := (Expr.lam name type allAnd binfo)
-        let newE := mkAppN t.getAppFn #[t.getAppArgs[0]!, newLam]
-        logInfo newE
-        --Reconstruct the CGen:
-        let newOuterLam := Expr.lam outer_name outer_type newE outer_binfo
-        --let newCGen := mkAppN e.getAppFn #[e.getAppArgs[0]!, newOuterLam]
-        logInfo newOuterLam
-        --Change the goal
-        let newEMvarId ← mkFreshExprMVar newOuterLam
-        --proofterming
-        let copyOfNewE ← mkFreshExprMVar newOuterLam
-        let simpTheorems ←  #[``and_comm,``and_assoc].foldlM (·.addConst ·) ({} : Meta.SimpTheorems)
-        --let ctx ← Simp.mkContext {} #[simpTheorems]
-        let (.some resultProof) ← NormCast.proveEqUsing simpTheorems origOuterLam newOuterLam | throwUnsupportedSyntax
-        -- let (simpResMV,simpStats) ← simpTarget copyOfNewE.mvarId! ctx
-        -- logInfo simpStats.usedTheorems.toArray[2]!.key
-        logInfo resultProof.proof?.get!
-        logInfo (← inferType resultProof.proof?.get!) --.proof?.get!
-        -- let newImpliesOld ← mkArrow newCGen e
-        -- let mvarIdImplies ← mkFreshExprMVar newImpliesOld (userName := `helper)
-        -- let proofTerm := mkApp mvarIdImplies newEMvarId
+      -- Create a new `rhs` with the appropriate grouping of conjuncts
+      let rhs' := mkAppN (.const ``And []) #[
+          varNotIn.foldr (fun a b => mkAppN (.const ``And []) #[a, b]) (.const ``True []),
+          varIn.foldr (fun a b => mkAppN (.const ``And []) #[a, b]) (.const ``True []),
+        ]
 
-        --assign the proof to get things to unify and be done with it?
-        mvarId.assign resultProof.proof?.get!
-        let e' ← instantiateMVars newOuterLam
-        -- let e'_mvar ← mkFreshExprMVar e'
-        -- replaceMainGoal [e'_mvar.mvarId!]
+      -- Create a new goal and prove it with aesop
+      let goalTy' ← mkAppM ``Eq #[rhs', rhs]
+      let g' ← mkFreshExprMVar (some goalTy')
+      let _ ← runTactic g'.mvarId! (← `(tactic| aesop))
 
-        -- let mNew ← mvarId.replaceTargetEq newCGen resultProof.proof?.get!
-        -- replaceMainGoal [mNew]
-
-        --run tauto to eliminate implication
-        -- let taut_stx ← `(tactic| tauto)
-        -- evalTactic taut_stx
-      else throwUnsupportedSyntax
-    else throwUnsupportedSyntax
+      -- NOTE: This was the trick to getting this to work. Checking that the goal type is equal to
+      -- the new goal type unifies the metavariables.
+      if ← isDefEq goalTy goalTy' then
+        closeMainGoal `partitionResult g'
+    | _ => pure ()
 
 def genTwoBetweens2 : CorrectGen (λ (v : Nat × Nat) => ∃ x, (2 ≤ x ∧ x ≤ 6) ∧ ∃ y, 2 ≤ y ∧ y ≤ 100 ∧ v = (x,y)) := by
-  -- apply convert (by rfl) (cbind _ _)
-  -- --sorry
-  -- · gapply (cbetween (by first | aesop | omega))
-  -- · intro a
-  --   apply convert (by partitionResult) (cbind _ _)
-  --   -- on_goal 3 =>
-  --   --   gapply (cbetween (by first | aesop | omega))
-  --   -- · intro a
-  --   --   obtain ⟨val_1, property⟩ := a
-  --   --   obtain ⟨left_1, right_1⟩ := property
-  --   --   simp_all only
-  --   --   apply synth_pure
-  --   all_goals sorry
-  sorry
+  apply convert (by rfl) (cbind _ _)
+  · gapply (cbetween (by omega))
+  · intro a
+    -- NOTE: Rather than try to prove the whole equality, I just did the funext / congrArg on the
+    -- outside. It doesn't matter too much. Note that we're now passing v directly.
+    apply convert (by funext v; apply congrArg; funext; partition_conjuncts v) (cbind _ _)
+    . cgenerator_search
+    . cgenerator_search
