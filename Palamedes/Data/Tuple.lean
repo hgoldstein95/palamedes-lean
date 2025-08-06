272b695f4eeb31dead41230ab6017cebe98be0e4
Daniel Sainati <sainatidaniel@gmail.com>
2025-08-06 17:08:07 -0400

    remove unused tuple lemmas

--- Palamedes/Data/Tuple.lean
+++ Palamedes/Data/Tuple.lean
@@ -16,17 +16,6 @@ def s_arbTuple
     funext (a, b)
     simp_all [g.property]
 
-def Tuple.unfold (a : Gen α) (b : Gen β) : Gen (α × β) :=
-  .bind a (fun a => .bind b (fun b => .ret (a, b)))
-
-theorem Tuple.support_unfold_congr {α β : Type} {a a' : Gen α} {b b' : Gen β}
-    {ha : support a = support a' }
-    {hb : support b = support b' } :
-    support (Tuple.unfold a b) = support (Tuple.unfold a' b') := by
-  unfold Tuple.unfold
-  simp [support]
-  aesop
-
 end CorrectGen
 
 end Gen
