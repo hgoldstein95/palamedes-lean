533909551d0a2c7a9e7d39c5aa58d7361e815e62
Daniel Sainati <sainatidaniel@gmail.com>
2026-03-22 22:36:51 -0400

    add min sketch for incr one

---
+++ Cobb/underapproximation_type/data/validation/list_incr_one_min_sketch/progMin.ml
@@ -0,0 +1,15 @@
+let rec incr_one_list_gen (s : int) (x : int) : int list =
+  if sizecheck s then Err
+  else
+    let (y : int) = int_gen () in
+    if y == x + 1 then
+      Err
+    else Exn
+
+let[@assert] incr_one_list_gen =
+  let s = ((0 <= v : [%v: int]) [@over]) in
+  let x = ((true : [%v: int]) [@over]) in
+  ((len v s && incr_one v
+    && (not (emp v)) #==> (fun ((u [@exists]) : int) -> hd v u && u == x + 1)
+    : [%v: int list])
+    [@under])
